19666d66b852389ff65baee8ac5676aec34d1e6f
anhv <anhv.ict91@gmail.com>
2020-08-15 07:41:39 +0700

    GH-298: update download_wikidata

--- underthesea/wiki.py
+++ underthesea/wiki.py
@@ -1,12 +1,30 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
 def download_wiki(title):
     pass
 
 
-def download_wikidata(title):
-    pass
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
 
-title = 'Việt_Nam'
+def download_wikidata_by_title(title):
+    sparql.setQuery('''
+    SELECT distinct ?item ?itemLabel ?itemDescription WHERE{  
+      ?item ?label "%s"@vi.  
+      ?article schema:about ?item .
+      ?article schema:name "%s"@vi .
+      ?article schema:inLanguage "vi" .
+      ?article schema:isPartOf <https://vi.wikipedia.org/>.	
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "vi". }    
+    }
+    ''' % (title, title))
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    print(results)
+
+
+title = 'Hà Nội'
 download_wiki(title)
 
-download_wikidata(title)
+download_wikidata_by_title(title)
